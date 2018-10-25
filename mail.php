5fed7e1f43fecfc267bc310b90aa4f6600775e2b
terrallewis <tldesignsprintandweb@gmail.com>
2018-10-25 06:28:35 -0400

    updated mail function for contact page

--- mail.php
+++ mail.php
@@ -1,31 +1,39 @@
 <?php
-    $to = 'demo@spondonit.com';
+
+if($_POST) {
+    $to = 'info@testcomm.com';
     $firstname = $_POST["fname"];
-    $email= $_POST["email"];
-    $text= $_POST["message"];
-    $phone= $_POST["phone"];
-    
+    $email = $_POST["email"];
+    $text = $_POST["message"];
+    $phone = $_POST["phone"];
+    $honeypot = $_POST['firstname'];
 
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-    $message ='<table style="width:100%">
+    $message = '<table style="width:100%">
         <tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
+            <td>' . $firstname . '  ' . $laststname . '</td>
         </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$phone.'</td></tr>
-        <tr><td>Text: '.$text.'</td></tr>
+        <tr><td>Email: ' . $email . '</td></tr>
+        <tr><td>phone: ' . $phone . '</td></tr>
+        <tr><td>Text: ' . $text . '</td></tr>
         
     </table>';
 
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'The message has been sent.';
+
+    if( $honeypot > 1 ){
+        return; //you may add code here to echo an error etc.
     }else{
-        echo 'failed';
+        if (@mail($to, $email, $message, $headers)) {
+            echo 'The message has been sent.';
+        } else {
+            echo 'failed';
+        }
     }
+
+}
 
 ?>
